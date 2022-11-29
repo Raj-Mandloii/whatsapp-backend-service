75633d61de6b6150162b20c300eb9bd1ebb289ec
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-11-29 17:21:01 +0530

    folder stru. changed

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 
 const express = require("express")
-const {connection} = require("./config/db")
-const {userController} = require("./routes/user.routes.js")
+const mongoose = require("mongoose");
+
+// const { connection } = require("./config/db")
+const { userController } = require("./routes/user.routes.js")
 const cors = require('cors');
 const parser = require("body-parser")
 const app = express();
 app.use(cors())
 require("dotenv").config()
 
-app.use(parser.json({extended: true}));
-app.use(parser.urlencoded({extended: true}));
-app.use("/",userController)
+app.use(parser.json({ extended: true }));
+app.use(parser.urlencoded({ extended: true }));
+app.use("/", userController)
 
 
 
@@ -18,11 +20,12 @@ app.use("/",userController)
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, async () => {
-    try{
-        await connection;
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+
         console.log("Connected to db")
     }
-    catch(err){
+    catch (err) {
         console.log("Error connnecting to DB")
         console.log(err)
     }
